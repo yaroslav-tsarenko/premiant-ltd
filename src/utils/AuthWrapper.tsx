44a1860d72701ef3b1e7263cc454c97d42249ff5
Yaroslav <yaroslav7v@gmail.com>
2024-12-29 22:06:19 +0200

    hotfixes

--- src/utils/AuthWrapper.tsx
+++ src/utils/AuthWrapper.tsx
@@ -4,32 +4,50 @@ import { cookies } from 'next/headers';
 export function authWrapper<P extends object>(Component: React.ComponentType<P>) {
     return function WrappedComponent(props: P) {
         const WrappedComponentAsync = async () => {
-            const cookieStore = await cookies();
-            const token = cookieStore.get('token')?.value;
+            try {
+                const cookieStore = await cookies();
+                const token = cookieStore.get('token')?.value;
 
-            const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user/get-user`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-                cache: 'no-store',
-            });
+                if (!token) {
+                    console.error('No token found');
+                    return (
+                        <UserProvider user={null as unknown as User}>
+                            <Component {...props} />
+                        </UserProvider>
+                    );
+                }
 
-            if (!response.ok) {
-                console.error('Failed to fetch user:', response.statusText);
+                const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user/get-user`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                    cache: 'no-store',
+                });
+
+                if (!response.ok) {
+                    console.error('Failed to fetch user:', response.statusText);
+                    return (
+                        <UserProvider user={null as unknown as User}>
+                            <Component {...props} />
+                        </UserProvider>
+                    );
+                }
+
+                const user = await response.json();
+
+                return (
+                    <UserProvider user={user.user as User}>
+                        <Component {...props} />
+                    </UserProvider>
+                );
+            } catch (error) {
+                console.error('Error in authWrapper:', error);
                 return (
                     <UserProvider user={null as unknown as User}>
                         <Component {...props} />
                     </UserProvider>
                 );
             }
-
-            const user = await response.json();
-
-            return (
-                <UserProvider user={user.user as User}>
-                    <Component {...props} />
-                </UserProvider>
-            );
         };
 
         return <WrappedComponentAsync />;
