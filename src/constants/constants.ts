c1581aff62ea32d732aed76a0f65dedb6c24a939
Yaroslav <yaroslav7v@gmail.com>
2025-01-02 14:17:04 +0200

    new changes

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,9 +1,9 @@
-export const FRONTEND_URL = "http://localhost:3000";
-export const BACKEND_URL = "http://localhost:8080";
+export const FRONTEND_URL = "https://premiant-ltd.vercel.app";
+export const BACKEND_URL = "https://premiant-ltd-be.onrender.com";
 
 /*
 http://localhost:3000
 http://localhost:8080
 https://premiant-ltd.vercel.app
-
+https://premiant-ltd-be.onrender.com
 */
