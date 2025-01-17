52c19bb63cf3a5632fef3485216f361471ea608f
Yaroslav <yaroslav7v@gmail.com>
2025-01-17 15:30:59 +0200

    font changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
 import { authWrapper } from "@/utils/AuthWrapper";
+import LenisScriptLoader from "@/utils/LenisScriptLoader";
 
 export const metadata: Metadata = {
     title: "Premiant LTD",
@@ -17,22 +18,28 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
 
     return (
         <html lang="en">
+        <head>
+            <title>Premiant LTD</title>
+            <link rel="preload" href="../assets/fonts/gropled-bold.otf" as="font" type="font/otf"
+                  crossOrigin="anonymous"/>
+        </head>
         <body>
+        <LenisScriptLoader/>
         <Header/>
         {children}
         <Footer
             footerLinks={[
-                { name: 'Главная', route: '/' },
-                { name: 'О Компании', route: '/about' },
-                { name: 'Инвесторам', route: '/#features' },
-                { name: 'FAQ', route: '/#faq' },
-                { name: 'Контакты', route: '/#address' },
-                { name: 'Поддержка', route: '/#address' },
+                {name: 'Главная', route: '/'},
+                {name: 'О Компании', route: '/about'},
+                {name: 'Инвесторам', route: '/#features'},
+                {name: 'FAQ', route: '/#faq'},
+                {name: 'Контакты', route: '/#address'},
+                {name: 'Поддержка', route: '/#address'},
             ]}
             contacts={[
-                { label: 'E-mail', value: 'premiantltd@gmail.com' },
-                { label: 'Phone', value: '+44 781 3243472' },
-                { label: 'Telegram', value: '@PremiantLTD' },
+                {label: 'E-mail', value: 'premiantltd@gmail.com'},
+                {label: 'Phone', value: '+44 781 3243472'},
+                {label: 'Telegram', value: '@PremiantLTD'},
             ]}>
         </Footer>
         </body>
