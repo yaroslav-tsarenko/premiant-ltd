6c8e8c201b135f5eeead830b252ce9d65a9ada91
Yaroslav <yaroslav7v@gmail.com>
2025-02-02 18:37:23 +0200

    new changes

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,5 @@
 export const FRONTEND_URL = "https://premiant.ltd";
-export const BACKEND_URL = "https://backend.premiant.ltd";
+export const BACKEND_URL = "https://premiant-be-7kaw.onrender.com";
 
 /*
 http://localhost:3000
